16c3667a3be22acc79c2c0dc7f1e714c3108b685
eyad elwakeel <128251830+eyadelwakeel@users.noreply.github.com>
2026-04-13 23:50:01 +0200

    update Owl Carousel

--- resources/views/website/layouts/app.blade.php
+++ resources/views/website/layouts/app.blade.php
@@ -93,8 +93,8 @@
         
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        
+        <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="{{ asset('website/assets/js/bootstrap.bundle.js') }}"></script>
         <script src="{{ asset('website/assets/js/bootstrap.bundle.min.js') }}"></script>
       
